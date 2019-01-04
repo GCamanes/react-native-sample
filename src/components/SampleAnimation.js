575307df9f90578b0a3e9494108e4df4f6b100be
Kevyn <kevyn.buchet@gmail.com>
2019-01-04 12:24:01 +0100

    Ajout d'une petite animation sympa

--- src/components/SampleAnimation.js
+++ src/components/SampleAnimation.js
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
-import { Image, View } from 'react-native';
+import { Image, Animated, Easing } from 'react-native';
 
 export default class SampleAnimation extends Component {
+  constructor(props){
+    super(props);
+
+    this.animatedValue = new Animated.Value(0);
+    this.spinValue = new Animated.Value(0);
+  }
+
+  componentDidMount () {
+    this.animate()
+    this.spin()
+  }
+
+  animate () {
+    this.animatedValue.setValue(0)
+    Animated.timing(
+      this.animatedValue,
+      {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.linear
+      }
+    ).start(() => this.animate())
+  }
+
+  spin () {
+    this.spinValue.setValue(0)
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 1,
+        duration: 4000,
+        easing: Easing.linear
+      }
+    ).start(() => this.spin())
+  }
 
   render() {
 
+    const movingMarginTop = this.animatedValue.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, 500, 0]
+    })
+
+    const movingMarginLeft = this.animatedValue.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, 200, 0]
+    })
+
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    })
+
+    const rotateX = this.animatedValue.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: ['0deg', '360deg', '0deg']
+    })
+
     return (
-      <View>
+      <Animated.View style={{marginTop: movingMarginTop, marginLeft: movingMarginLeft, alignItems: 'center', transform: [{rotate : spin},{rotateX}]}}>
         <Image source={require('../../assets/NY1.jpg')} />
-      </View>
+      </Animated.View>
     );
   }
 }
